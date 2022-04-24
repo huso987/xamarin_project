3232e1cf5138f81d3f34a7a4194295c2fa33f8ca
D-hcr <hcrdz19@gmail.com>
2022-04-25 00:07:49 +0300

    commit 1
    search bar , carouselview   ve ana sayfaya bazı kategoriler eklendi.

---
+++ Migros/Migros/Views/Anasayfa.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Migros.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Anasayfa : ContentPage
+    {
+        public Anasayfa()
+        {
+            InitializeComponent();
+
+            var names = new List<string>
+            {
+
+                "mohammed", "ahmed","marven","cihad"
+            };
+
+            var images = new List<string>
+            {
+                "https://migros-dali-storage-prod.global.ssl.fastly.net/hemen/banner/main_page_slider/25112/27146-_hemenramazan_3420x900-72190d.png",
+                "https://migros-dali-storage-prod.global.ssl.fastly.net/hemen/banner/main_page_slider/25392/27426-web_hemen_meyvesebze-f6faca.png",
+                "https://migros-dali-storage-prod.global.ssl.fastly.net/hemen/banner/main_page_slider/21710/23744-791_hemen_web_banner_ucretsiz_teslimat-4518f1.png",
+                "https://migros-dali-storage-prod.global.ssl.fastly.net/hemen/banner/main_page_slider/20901/22935-784_hemen_web_slider_20_tl_indirim(1)-b98bf3.png"
+            };
+
+
+            Slider.ItemsSource = images;
+        }
+    }
+}
